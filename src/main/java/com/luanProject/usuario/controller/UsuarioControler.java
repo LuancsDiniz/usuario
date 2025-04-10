f15de462caf4721d57ca0bee1cab598bdd6fc16c
Luan Diniz <luancsdiniz@outlook.com>
2025-04-10 19:00:50 -0300

    Adicionado método que salva dados do usuário

---
+++ src/main/java/com/luanProject/usuario/controller/UsuarioControler.java
@@ -0,0 +1,23 @@
+package com.luanProject.usuario.controller;
+
+import com.luanProject.usuario.business.UsuarioService;
+import com.luanProject.usuario.business.dto.UsuarioDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
+public class UsuarioControler {
+
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> salvaUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+        return ResponseEntity.ok(usuarioService.salvaUsuario(usuarioDTO));
+    }
+}
