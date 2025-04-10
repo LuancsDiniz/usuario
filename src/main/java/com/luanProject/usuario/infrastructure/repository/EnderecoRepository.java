055c6141509ada760633c914f433671bd2fd4824
Luan Diniz <luancsdiniz@outlook.com>
2025-04-10 14:27:47 -0300

    Adicionadas as classes de security

---
+++ src/main/java/com/luanProject/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,10 @@
+package com.luanProject.usuario.infrastructure.repository;
+
+import com.projetoLuan.aprendendospring.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+
+}
---
+++ src/main/java/com/luanProject/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.luanProject.usuario.infrastructure.repository;
+
+import com.projetoLuan.aprendendospring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
